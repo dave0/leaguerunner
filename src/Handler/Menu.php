0ca8b2e5245edbae4ca5894c4d9be1af9a4be8c4
Dave O'Neill <dmo@dmo.ca>
2002-10-03 06:26:39 +0000

    - Rewrite menu generation code to suck less, and be easier to extend.

--- src/Handler/Menu.php
+++ src/Handler/Menu.php
@@ -56,134 +56,25 @@ class Menu extends Handler
 	function process ()
 	{
 		global $session;
+
+		$id =  $session->attr_get("user_id");
+		
 		$this->set_template_file("Menu.tmpl");
-		$this->tmpl->assign("menu_box_rows",
-				array(
-					$this->manage_account(),
-					$this->manage_teams(),
-					$this->manage_tiers(),
-					$this->manage_leagues(),
-					$this->manage_system(),
-				)
-		);
+		
 		$this->tmpl->assign("user_name", join(" ",array(
 			$session->attr_get("firstname"),
 			$session->attr_get("lastname")
 			)));
+		$this->tmpl->assign("user_id", $id);
+
+		/* Fetch team info */
+		$teams = get_teams_for_user($id);
+		if($this->is_database_error($teams)) {
+			return false;
+		}
+		$this->tmpl->assign("teams", $teams);
+		
 		return true;
 	}
-
-	/**
-	 * Generate the account menu
-	 * @access private
-	 * @return array
-	 */
-	function manage_account ()
-	{
-		global $session;
-		$ops = array(
-			array(
-				'title' => "View/Edit My Account",
-				'url_append' => '?op=person_view&id=' . $session->attr_get("user_id")
-			),
-			array(
-				'title' => "Change Password",
-				'url_append' => '?op=person_password'
-			),
-			array(
-				'title' => "Log Out",
-				'url_append' => '?op=logout'
-			),
-		);
-		return array(
-			'title' => "My Account",
-			'content' => $this->generate_menu_html($ops),
-		);
-	}
-	
-	function manage_teams ()
-	{
-		global $session, $DB;
-
-		/* TODO: replace with get_teams_for_user() */
-		$sth = $DB->prepare("SELECT t.team_id, 
-            t.name AS team_name, 
-            if(t.captain_id = r.player_id, 
-                'captain', 
-                if(t.assistant_id = r.player_id, 
-                    'assistant',
-                    if(r.status = 'confirmed',
-                        'player',
-                        'requested'))) as position,
-            l.league_id
-        FROM 
-            team t,
-            teamroster r,
-            leagueteams l
-        WHERE 
-            r.team_id = t.team_id AND 
-            t.team_id = l.team_id AND
-            r.player_id = ?");
-		$res = $DB->execute($sth,array($session->attr_get("user_id")));
-		if(DB::isError($res)) {
-			$output = "No teams";
-		} else {
-			$output = "<table>";
-			while($row = $res->fetchRow(DB_FETCHMODE_ASSOC)) {
-				$output .= $this->gen_team_row($row['team_name'],$row['position'],$row['team_id']);
-			}
-			$output .= "</table>";
-		}
-
-		return array(
-			'title' => "My Teams",
-			'content' => $output, 
-		);
-	}
-
-	function gen_team_row($name,$pos,$id)
-	{
-		$rv = "<tr><td>$pos</td><td>on</td><td>$name</td>";
-		$rv .= "<td><a href='" . $GLOBALS['APP_CGI_LOCATION']. "?op=team_view&id=$id'>view/edit</a></td>";
-		return $rv;
-	}
-	
-	function manage_tiers ()
-	{
-	}
-	
-	function manage_leagues ()
-	{
-	}
-
-	function manage_system ()
-	{
-	}
-
-	/**
-	 * Generate the HTML for a menu box
-	 *
-	 * Generates the HTML for one of the menu boxes on the main menu
-	 * screen. The argument given is an array of arrays, containing:
-	 *   title (string for menu box title)
-	 *   url_append (string to append to the CGI url)
-	 *
-	 * @access private
-	 * @param  array	Array of operations supported
-	 * @return string  HTML code to insert into menu box
-	 */
-	function generate_menu_html ( $available_ops )
-	{
-		global $session_id;
-		global $APP_CGI_LOCATION;
-		$s = "<table>";
-		reset($available_ops);
-		while (list($key, $val) = each($available_ops)) {
-			$s .= "<tr><td><a href='" . $APP_CGI_LOCATION . $val['url_append']."'>".$val['title']."</a></td></tr>\n";
-		}
-		$s .= "</table>";
-		return $s;
-	}
-
 }
 ?>
