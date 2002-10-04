60687e5489c30c5d2d99088fddea32c6ad70c227
Dave O'Neill <dmo@dmo.ca>
2002-10-04 04:11:36 +0000

    - updated main menu to have the same links from the old version.

--- src/Handler/Menu.php
+++ src/Handler/Menu.php
@@ -17,6 +17,13 @@ class Menu extends Handler
 	function initialize ()
 	{
 		$this->name = 'LeagueRunner Menu';
+
+		$this->_permissions = array(
+			"league_admin"  => false,
+			"league_create" => false,
+			"user_admin"    => false,
+		);
+		
 		return true;
 	}
 
@@ -34,13 +41,22 @@ class Menu extends Handler
 		global $session;
 		
 		/* Anyone with a valid session id has permission */
-		if($session->is_valid()) {
-			return true;
+		if(!$session->is_valid()) {
+			/* If no session, it's error time. */
+			$this->name = "Not Logged In";
+			$this->error_text = gettext("Your session has expired.  Please log in again");
+			return false;
 		}
-		/* If no session, it's error time. */
-		$this->name = "Not Logged In";
-		$this->error_text = gettext("Your session has expired.  Please log in again");
-		return false;
+
+		/* Administrator can do all */
+		if($session->attr_get('class') == 'administrator') {
+			while(list($key,) = each($this->_permissions)) {
+				$this->_permissions[$key] = true;
+			}
+		}
+		reset($this->_permissions);
+		
+		return true;
 	}
 
 	/**
@@ -55,7 +71,7 @@ class Menu extends Handler
 	 */
 	function process ()
 	{
-		global $session;
+		global $session, $DB;
 
 		$id =  $session->attr_get("user_id");
 		
@@ -73,6 +89,37 @@ class Menu extends Handler
 			return false;
 		}
 		$this->tmpl->assign("teams", $teams);
+
+		/* Fetch leagues coordinated */
+		$leagues = $DB->getAll("
+			SELECT 
+				league_id AS id,
+				name,
+				tier
+		  	FROM 
+				league 
+		    WHERE 
+				coordinator_id = ? OR (alternate_id = ? AND NOT alternate_id = 1)
+		  	ORDER BY name, tier",
+			array($id,$id), 
+			DB_FETCHMODE_ASSOC);
+			
+		if($this->is_database_error($leagues)) {
+			return false;
+		}
+			
+		if(count($leagues) > 0) {
+			$this->_permissions['league_admin'] = true;
+			$this->tmpl->assign("leagues", $leagues);
+		}
+		
+		/* ... and set permissions flags */
+		while(list($key,$val) = each($this->_permissions)) {
+			if($val) {
+				error_log("Set perm for $key to true");
+				$this->tmpl->assign("perm_$key", true);
+			}
+		}
 		
 		return true;
 	}
