55714c0c618946bb3f15c0424518fde75e6dc58c
Dave O'Neill <dmo@dmo.ca>
2003-04-16 14:43:33 +0000

    Need to set season=none to have this league show up in listing

--- setup/db/mysql_initialdata.sql
+++ setup/db/mysql_initialdata.sql
@@ -1,5 +1,5 @@
 use leaguerunner;
 
 INSERT INTO person (username,password,firstname,lastname,class) VALUES ('admin',MD5('admin'), 'System', 'Administrator','administrator');
-INSERT INTO league (name,coordinator_id,allow_schedule) VALUES ('Inactive Teams', 1, 'N');
+INSERT INTO league (name,coordinator_id,season,allow_schedule) VALUES ('Inactive Teams', 1, 'none', 'N');
 
