c5c5c3f71ddb10dc3ab8043b120a2018b2df2c3c
Dave O'Neill <dmo@dmo.ca>
2005-03-19 14:37:18 +0000

    Actually tested the install instructions.  These updates are the result

--- db/mysql_initialdata.sql
+++ db/mysql_initialdata.sql
@@ -1,7 +1,5 @@
-use leaguerunner;
-
 INSERT INTO person (username,password,firstname,lastname,class,status) VALUES ('admin',MD5('admin'), 'System', 'Administrator','administrator','active');
-INSERT INTO league (name,season,allow_schedule) VALUES ('Inactive Teams', 'none', 'N');
+INSERT INTO league (name,season,schedule_type) VALUES ('Inactive Teams', 'none', 'N');
 INSERT INTO leaguemembers (league_id, player_id, status) VALUES (1,1,'coordinator');
 
 -- 
