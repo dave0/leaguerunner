e3ab816c37cd4adf1130fd37ad3d0063679a86d7
Dave O'Neill <dmo@dmo.ca>
2005-05-03 03:05:42 +0000

    - add spirit note to migration script

--- db/migrate_2_0_PRE_1-to-2_0_PRE_4.sql
+++ db/migrate_2_0_PRE_1-to-2_0_PRE_4.sql
@@ -6,3 +6,12 @@ alter table field add latitude double after location_province;
 alter table field add longitude double after latitude;
 
 alter table person add shirtsize varchar(50) after year_started;
+
+-- Note to coordinator
+INSERT INTO question (qkey,genre,question,qtype,required,sorder) VALUES (
+	'CommentsToCoordinator',
+	'team_spirit',
+	'Do you have any comments on this game you would like to bring to the coordinator''s attention?', 
+	'freetext',
+	'N',
+	'5');
