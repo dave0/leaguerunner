cc787457f5745bcc0130d9bc2f886f67ccf1fb33
Tony Argentina <tonysa@ulty.ca>
2006-06-14 14:12:18 +0000

    Adding functionality:  ability to remove game results (ie: undo rank change, ratings change, win/loss/tie/gf/ga/sotg change, etc, but does not actually delete the game.  So, game results can be re-entered or fixed.

--- src/Handler/Game.php
+++ src/Handler/Game.php
@@ -30,6 +30,12 @@ function game_dispatch()
 			$obj->game = game_load( array('game_id' => $id) );
 			$obj->league = league_load( array('league_id' => $obj->game->league_id) );
 			break;
+		case 'removeresults':
+			// Allow deletion of a game (not gameslot!)
+			$obj = new GameRemoveResults;
+			$obj->game = game_load( array('game_id' => $id) );
+			$obj->league = league_load( array('league_id' => $obj->game->league_id) );
+			break;
 /* TODO:
 		case 'reschedule':
 			# TODO: move a game from one gameslot to another.
@@ -180,6 +186,7 @@ function game_add_to_menu( &$league, &$game )
 		menu_add_child("$league->fullname/schedule/$game->game_id", "$league->fullname/schedule/$game->game_id/edit", "edit game", array('link' => "game/edit/$game->game_id"));
 #		menu_add_child("$league->fullname/schedule/$game->game_id", "$league->fullname/schedule/$game->game_id/reschedule", "reschedule game", array('link' => "game/reschedule/$game->game_id"));
 		menu_add_child("$league->fullname/schedule/$game->game_id", "$league->fullname/schedule/$game->game_id/delete", "delete game", array('link' => "game/delete/$game->game_id"));
+		menu_add_child("$league->fullname/schedule/$game->game_id", "$league->fullname/schedule/$game->game_id/removeresults", "remove results", array('link' => "game/removeresults/$game->game_id"));
 	}
 }
 
@@ -1423,6 +1430,230 @@ class GameDelete extends Handler
 
 		if ( ! $this->game->delete() ) {
 			error_exit("Could not successfully delete the game");
+		}
+
+		return true;
+	}
+	
+}
+
+######################################################################
+# To remove just the results of a game... ie: teams enter wrong scores
+# This will UNDO any change to rank, ratings, wins/losses/ties, goals for
+#  goals against, SOTG, etc.....
+# After this, the game can be re-entered since the game itself is not deleted.
+# 
+class GameRemoveResults extends Handler
+{
+	var $game;
+	
+	function has_permission ()
+	{
+		global $session;
+		return $session->has_permission('game','view', $this->game);
+	}
+	
+	function process ()
+	{
+		global $session;
+		if(!$this->game) {
+			error_exit("That game does not exist");
+		}
+
+		if( arg(1) == 'removeresults' ) {
+			if( $session->is_admin() ) {
+				$this->can_edit = true;
+			}
+	
+			if( $session->is_coordinator_of($this->game->league_id)) {
+				$this->can_edit = true;
+			}
+		} else {
+			$this->can_edit = false;
+		}
+
+		$this->title = "Game Remove Results";
+		
+		$this->setLocation(array(
+			"$this->title &raquo; Remove Results for Game " . $this->game->game_id => 0));
+
+		$edit = $_POST['edit'];
+
+		switch($edit['step']) {
+			case 'confirm':
+				$rc = $this->generateConfirm( $this->game, $edit );
+				break;
+			case 'perform':
+				$this->perform( $edit );
+				local_redirect(url("schedule/view/" . $this->league->league_id));
+				break;
+			default:
+				$rc = $this->generateForm( );
+		}
+
+		return $rc;
+	}
+	
+	function generateForm ( ) 
+	{
+		global $session;
+		# Alias, to avoid typing.  Bleh.
+		$game = &$this->game;
+		$league = &$this->league;
+
+		$game->load_score_entries();
+		
+		$output = form_hidden('edit[step]', 'confirm');
+		
+		$output .= form_item("Game ID", $game->game_id);
+
+		$teams = $league->teams_as_array();
+		/* Now, since teams may not be in league any longer, we need to force
+		 * them to appear in the pulldown
+		 */
+		$teams[$game->home_id] = $game->home_name;
+		$teams[$game->away_id] = $game->away_name;
+
+		$output .= form_item("League/Division", l($league->fullname, "league/view/$league->league_id"));
+
+		$output .= form_item( "Home Team", l($game->home_name,"team/view/$game->home_id"));
+		$output .= form_item( "Away Team", l($game->away_name,"team/view/$game->away_id"));
+
+		if( $this->can_edit ) {
+			$note = "To edit time, date, or location, use the 'reschedule' link";
+		}
+		$output .= form_item("Date and Time", "$game->game_date, $game->game_start until $game->game_end", $note);
+
+		$field = field_load( array('fid' => $game->fid) );
+		$output .= form_item("Location",
+			l("$field->fullname ($game->field_code)", "field/view/$game->fid"), $note);
+
+		$output .= form_item("Game Status", $game->status);
+	
+		$output .= form_item("Round", $game->round);
+
+		$spirit_group = '';
+		$score_group = '';
+
+		/*
+		 * Now, for scores and spirit info.  Possibilities:
+		 *  - game has been finalized:
+		 *  	- everyone can see scores
+		 *  	- coordinator can edit scores/spirit
+		 *  - game has not been finalized
+		 *  	- players only see "not yet submitted"
+		 *  	- captains can see submitted scores
+		 *  	- coordinator can see everything, edit final scores/spirit
+		 */
+
+
+		if( ! $game->approved_by) {
+			// if the game is not finalized, results cannot be removed yet...
+			error_exit("The game is not finalized, and so results cannot be removed at this time.");
+      }
+
+
+
+
+
+
+		// If we're not editing, display score.  If we are, 
+		// it will show up below.
+		switch($game->status) {
+			case 'home_default':
+				$home_status = " (defaulted)";
+				break;
+			case 'away_default':
+				$away_status = " (defaulted)";
+				break;
+			case 'forfeit':
+				$home_status = " (forfeit)";
+				$away_status = " (forfeit)";
+				break;
+		}
+		$score_group .= form_item("Home ($game->home_name) Score", "$game->home_score $home_status");
+		$score_group .= form_item("Away ($game->away_name) Score", "$game->away_score $away_status");
+	
+		$score_group .= form_item("Rating Points", $game->rating_points,"Rating points transferred to winning team from losing team");
+	
+		switch($game->approved_by) {
+			case APPROVAL_AUTOMATIC:
+				$approver = 'automatic approval';
+				break;
+			case APPROVAL_AUTOMATIC_HOME:
+				$approver = 'automatic approval using home submission';
+				break;
+			case APPROVAL_AUTOMATIC_AWAY:
+				$approver = 'automatic approval using away submission';
+				break;
+			case APPROVAL_AUTOMATIC_FORFEIT:
+				$approver = 'game automatically forfeited due to lack of score submission';
+				break;
+			default:
+				$approver = person_load( array('user_id' => $game->approved_by));
+				$approver = l($approver->fullname, "person/view/$approver->user_id");
+		}
+		$score_group .= form_item("Score Approved By", $approver);		
+
+
+
+		$output .= form_group("Scoring", $score_group);
+	
+		if( $session->has_permission('game','view',$game,'spirit') ) {
+		
+			$formbuilder = formbuilder_load('team_spirit');
+			$ary = $game->get_spirit_entry( $game->home_id );
+			if( $ary ) {
+				$formbuilder->bulk_set_answers( $ary );
+			}
+			$home_spirit_group = $formbuilder->render_viewable( $ary );
+		
+			$formbuilder->clear_answers();
+			$ary = $game->get_spirit_entry( $game->away_id );
+			if( $ary ) {
+				$formbuilder->bulk_set_answers( $ary );
+			}
+			$away_spirit_group = $formbuilder->render_viewable( $ary );
+			
+			$output .= form_group("Spirit assigned TO home ($game->home_name)", $home_spirit_group);
+			$output .= form_group("Spirit assigned TO away ($game->away_name)", $away_spirit_group);
+		}
+
+		if( $this->can_edit ) {
+			$output .= "<p><font color='red'>If you click <b>submit</b>, you will <b>remove all results</b> for this game!</font></p>";
+			$output .= para(form_submit("submit") . form_reset("reset"));
+		}
+		return $script . form($output);
+	}
+	
+	function generateConfirm ( $game, $edit )
+	{
+
+		if( ! $this->can_edit ) {
+			error_exit("You do not have permission to remove results for this game");
+		}
+	
+		$output = para( "You have requested to <b>remove results</b> for the game: <b>$game->game_date $game->game_start between $game->home_name and $game->away_name</b>.  ");
+		$output .= para( "If this is correct, please click 'Submit' to continue.  If not, use your back button to return to the previous page.");
+
+		$output .= form_hidden('edit[step]', 'perform');
+		
+		$output .= para(form_submit('submit'));
+
+		return form($output);
+	}
+	
+	
+	function perform ( $edit )
+	{
+		global $session;
+		
+		if( ! $this->can_edit ) {
+			error_exit("You do not have permission to remove results for this game");
+		}
+
+		if ( ! $this->game->removeresults() ) {
+			error_exit("Could not successfully remove results for the game");
 		}
 
 		return true;
