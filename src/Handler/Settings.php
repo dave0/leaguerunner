45519ce4250e095cf39b406e9f236e0220f87ae6
Greg Schmidt <greg@trawna.com>
2008-11-20 20:15:35 +0000

    Added options for score approval (default scores, spirit penalties) and narrower displays.

--- src/Handler/Settings.php
+++ src/Handler/Settings.php
@@ -57,13 +57,23 @@ function global_settings()
 
 	$group .= form_textfield("Location of privacy policy (URL)", 'edit[privacy_policy]', variable_get('privacy_policy', 'http://www.ocua.ca/node/17'), 60, 120, "The web-accessible URL where the organization's privacy policy is located. Leave blank if you don't have an online privacy policy.");
 
-	$group .= form_textfield("Google Maps API Key", 'edit[gmaps_key]', variable_get('gmaps_key', ''), 60, 120, "An API key for the Google Maps API - see http://www.google.com/apis/maps/signup.html.  Required for rendering custom Google Maps");
+	$group .= form_textfield("Google Maps API Key", 'edit[gmaps_key]', variable_get('gmaps_key', ''), 60, 120, 'An API key for the <a href="http://www.google.com/apis/maps/signup.html">Google Maps API</a>. Required for rendering custom Google Maps');
 
 	$output = form_group("Site configuration", $group);
 
-	$group = form_select("Current Season", "edit[current_season]", variable_get("current_season","Summer"), getOptionsFromEnum('league','season'), "Season of play currently in effect");
+	$group = form_select("Current Season", 'edit[current_season]', variable_get('current_season','Summer'), getOptionsFromEnum('league','season'), "Season of play currently in effect");
 
 	$output .= form_group("Season Information", $group);
+
+	$group = form_textfield('Spirit penalty for not entering score', 'edit[missing_score_spirit_penalty]', variable_get('missing_score_spirit_penalty', 3), 10, 10);
+
+	$group .= form_textfield('Winning score to record for defaults', 'edit[default_winning_score]', variable_get('default_winning_score', 6), 10, 10);
+
+	$group .= form_textfield('Losing score to record for defaults', 'edit[default_losing_score]', variable_get('default_losing_score', 0), 10, 10);
+
+	$group .= form_radios('Transfer ratings points for defaults', 'edit[default_transfer_ratings]', variable_get('default_transfer_ratings', 0), array('Disabled', 'Enabled'));
+
+	$output .= form_group("Game Finalization", $group);
 
 	return settings_form($output);
 }
@@ -76,7 +86,9 @@ function feature_settings()
 
 	$group .= form_radios("Clean URLs", "edit[clean_url]", variable_get("clean_url", 0), array("Disabled", "Enabled"), "Enable or disable clean URLs.  If enabled, you'll need <code>ModRewrite</code> support.  See also the <code>.htaccess</code> file in Leaguerunner's top-level directory.");
 
-	$group .= form_radios("Use Post-Nuke authentication", "edit[postnuke]", variable_get("postnuke", 0), array("Disabled", "Enabled"), "If enabled, Leaguerunner will use your Post-Nuke user database for authentication. Everything to do with passwords will be hidden.");
+	$group .= form_radios("Use Post-Nuke authentication", "edit[postnuke]", variable_get("postnuke", 0), array("Disabled", "Enabled"), "If enabled, Leaguerunner will use your Post-Nuke user database for user names, passwords and email addresses. Everything to do with passwords and logins will be hidden.");
+
+	$group .= form_radios("Narrow display", "edit[narrow_display]", variable_get("narrow_display", 0), array("Disabled", "Enabled"), "If enabled, various displays will be adjusted for horizontal compactness. This is most useful when using Post-Nuke, as its left menu block eats up valuable real estate.");
 
 	$group .= form_radios("Lock sessions to initiating IP address", "edit[session_requires_ip]", variable_get("session_requires_ip", 1), array("Disabled", "Enabled"), "If enabled, session cookies are only accepted if they come from the same IP as the initial login.  This adds a bit of security against cookie theft, but causes problems for users behind a firewall that routes HTTP requests out through multiple IP addresses.  Recommended setting is to enable unless you notice problems. This setting is ignored if Post-Nuke authentication is enabled.");
 
