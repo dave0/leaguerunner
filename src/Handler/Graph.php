848621b71bcaec579b5b975fd9eb5af65ce4a66c
Dave O'Neill <dmo@dmo.ca>
2009-05-14 01:34:19 +0000

    Remove non-working code

--- src/Handler/Graph.php
+++ src/Handler/Graph.php
@@ -19,9 +19,6 @@ function graph_dispatch()
 {
 	$op = arg(1);
 
-	if(!$_GET['image'] == 'yes') {
-		return new GraphWrapper;	
-	}
 	switch($op) {
 		case 'teamspirit':
 			$obj = new GraphTeamSpirit;
@@ -36,140 +33,6 @@ function graph_dispatch()
 			$obj = null;
 	}
 	return $obj;
-}
-
-class GraphWrapper extends Handler
-{
-	function has_permission()
-	{
-		return true;
-	}
-
-	function process ()
-	{
-		$op = arg(1);
-		switch($op) {
-			case 'leaguerank':
-				$league = league_load( array( 'league_id' => arg(2)));
-				league_add_to_menu( $league );
-				break;	
-			case 'teamrank':
-				$team_ids = explode("/", $_GET["q"]);
-				array_shift($team_ids);  // Remove handler
-				array_shift($team_ids);  // remove op
-				foreach($team_ids as $id) {
-					$team = team_load( array('team_id' => $id) );
-					if( !$team ) {
-						continue;
-					}
-					team_add_to_menu($team);
-				}
-				break;
-			default:
-				break;
-		}
-	
-		$uri = request_uri();
-		if(preg_match("/\?/", $uri) ) {
-			$uri .= "&image=yes";
-		} else {
-			$uri .= "?image=yes";
-		}
-		return "<img src='$uri' />";
-	}
-}
-
-class GraphTeamSpirit extends Handler
-{
-	function has_permission ()
-	{
-		global $lr_session;
-		return $lr_session->has_permission('team','view');
-	}
-
-	function process ()
-	{
-		global $lr_session;
-		
-		$graph = new Graph(600,400,'auto', LR_GRAPH_TIMEOUT);
-		$graph->SetScale("intint");
-		$graph->yscale->SetGrace(10);
-		$graph->img->SetMargin(40,20,20,40);
-		$graph->legend->SetPos(0.5,0.99,'center','bottom');
-		$graph->legend->SetLayout(LEGEND_HOR);
-
-		$graph->title->Set("Team Spirit by Week");
-#		$graph->xaxis->title->Set("Week");
-#		$graph->xaxis->SetPos('max');
-		$graph->xaxis->Hide();
-		$graph->yaxis->title->Set("SOTG");
-
-		$team_ids = explode("/", $_GET["q"]);
-		array_shift($team_ids);  // Remove handler
-		array_shift($team_ids);  // remove op
-
-		foreach($team_ids as $id) {
-			$team = team_load( array('team_id' => $id) );
-			if( !$team ) {
-				error_exit("That is not a valid team ID");
-			}
-			if( ! $lr_session->is_coordinator_of($team->league_id) ) {
-				error_exit("You do not have permission to view that team's spirit");
-			}
-			$this->add_to_graph($graph, $team);
-		}
-		header("Content-type: image/png");
-		$graph->Stroke();
-		exit;
-	}
-	
-	function add_to_graph( &$graph, &$team) 
-	{
-		// Get games
-		$games = game_load_many( array('either_team' => $team->team_id) );
-		if($games) {
-			$ydata = array();
-			$y2data= array();
-			$running_total = 0;
-			$count = 0;
-			foreach($games as $game) {
-				if( ! $game->is_finalized() ) {
-					continue;
-				}
-	#			if( $game->status != 'normal') {
-	#				continue;
-	#			}
-				$count++;
-				$sotg = $game->get_spirit_numeric($team->team_id);
-				$running_total += $sotg;
-				$ydata[] = $sotg;
-				$y2data[] = ($running_total / $count);
-		#		$y3data[] = abs($game->home_score - $game->away_score);
-			}
-			
-			// Create the linear plot
-			$plot = new LinePlot($ydata);
-			$plot->SetColor(getcolour($team->rank));
-			$plot->mark->SetType(getsymbol($team->rank));
-			$plot->mark->SetColor(getcolour($team->rank));
-			$plot->value->Show();
-			$plot->SetLegend($team->name . " Per-Game");
-			$graph->Add($plot);
-			
-			$plot2 = new LinePlot($y2data);
-			$plot2->SetColor(getcolour($team->rank));
-			$plot2->SetWeight(3);
-			$plot2->SetLegend($team->name . " Average");
-			$graph->Add($plot2);
-			
-	#		$plot3 = new LinePlot($y3data);
-	#		$plot3->mark->SetType(getsymbol($team->rank));
-	#		$plot3->mark->SetColor(getcolour($team->rank));
-	#		$plot3->SetLegend($team->name . " ScoreDiff");
-	#		$graph->Add($plot3);
-		}
-	}
-
 }
 
 class GraphPlayerSkill extends Handler
@@ -250,19 +113,4 @@ class GraphRosterSize extends Handler
 	}
 }
 
-function getcolour ( $id )
-{
-	$colours = array('blue','green','red','black');
-	return $colours[ $id % count($colours) ];
-}
-function getsymbol ( $id )
-{
-	$symbols = array(MARK_SQUARE,MARK_UTRIANGLE,MARK_DTRIANGLE,MARK_DIAMOND, MARK_CIRCLE, MARK_FILLEDCIRCLE, MARK_CROSS, MARK_STAR, MARK_X, MARK_LEFTTRIANGLE,MARK_RIGHTTRIANGLE);
-	return $symbols[ $id % count($symbols) ];
-}
-function _cb_invert($value)
-{
-	return sprintf('%d', round(-$value));
-#	return round(-$value);
-}
 ?>
