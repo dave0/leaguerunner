6e3dd53a70facec0f159c899df0454ac33df6ccd
Dave O'Neill <dmo@dmo.ca>
2009-05-21 05:34:40 +0000

    Simplified output for "Games Today" page

--- src/Handler/API.php
+++ src/Handler/API.php
@@ -48,31 +48,28 @@ class APIGamesToday extends Handler
 		$sth->execute( array( strftime('%Y-%m-%d', $now) ) );
 		list($game_count, $distinct_end_times) = $sth->fetch();
 
+		$timecap_html = '';
 		if( ! $game_count ) {
-			$game_count = 'No';
+			$gamecount_html = '<b>No games today</b>';
+		} else {
+			/* Also, take a stab at guessing timecap.  Since only summer usually
+			* has a "default" timecap, we will only display for that season.
+			*/
+			$season = strtolower(variable_get('current_season', "Summer"));
+			if( $season == 'summer' ) {
+				$timecap_html .= '<span id="timecap">Timecap is <b>';
+				$timecap_html .= local_sunset_for_date( $now );
+				$timecap_html .= '</b>';
+				if( $distinct_end_times > 1 ) {
+					$timecap_html .= ' (some games may differ)';
+				}
+				$timecap_html .= '</span>';
+			}
+			$gamecount_html = "<b><a href=\"$day_url\">$game_count games today</a></b>";
 		}
 
-		/* Also, take a stab at guessing timecap.  Since only summer usually
-		 * has a "default" timecap, we will only display for that season.
-		 */
-		$season = strtolower(variable_get('current_season', "Summer"));
-		$timecap_html = '';
-		if( $season == 'summer' && ($game_count > 0) ) {
-			$timecap_html .= '<div id="timecap">Timecap is <b>';
-			$timecap_html .= local_sunset_for_date( $now );
-			$timecap_html .= '</b>';
-			if( $distinct_end_times > 1 ) {
-				$timecap_html .= ' (unless otherwise specified in Leaguerunner)';
-			}
-			$timecap_html .= '</div>';
-		}
 		print <<<END_HTML
-<html>
- <body>
-	<div id="gamestoday"><b>League Games Today:</b> <a href="$day_url">$game_count games today</a></div>
-	$timecap_html
- </body>
-</html>
+<html><body><div id="gamestoday">$gamecount_html $timecap_html</div></body></html>
 END_HTML;
 		exit; // Don't return -- we don't want themed HTML wrapped around our output
 	}
