a295f3d349aa4ae384e68a507941908edc500759
Dave O'Neill <dmo@dmo.ca>
2010-06-13 20:27:57 -0400

    Templating: ical export

--- src/Handler/team/ical.php
+++ src/Handler/team/ical.php
@@ -1,11 +1,6 @@
 <?php
-
 require_once('Handler/TeamHandler.php');
 
-/**
- * RMK April 2008
- * Team schedule as ical handler
- */
 class team_ical extends TeamHandler
 {
 	function has_permission ()
@@ -14,14 +9,15 @@ class team_ical extends TeamHandler
 		return $lr_session->has_permission('team','view schedule', $this->team->team_id);
 	}
 
-	// Does not return, as we don't want normal LR theme output
-	// Will output in target format (ical)
 	function process ()
 	{
 		global $CONFIG;
-		$timezone = 'TZID=' .  $CONFIG['localization']['local_tz'];
 
-		$my_team = $this->team->name;
+		$this->template_name = 'pages/team/ical.tpl';
+
+		$this->smarty->assign('team', $this->team);
+		$this->smarty->assign('short_league_name', variable_get('app_org_short_name', 'League'));
+		$this->smarty->assign('timezone', $CONFIG['localization']['local_tz']);
 
 		/*
 		 * Grab schedule info
@@ -33,93 +29,22 @@ class team_ical extends TeamHandler
 		// Prevent caching
 		header("Cache-Control: no-cache, must-revalidate");
 
-		// get league name for iCalendar name
-		$short_league_name = variable_get('app_org_short_name', 'League');
-
 		// get domain URL for signing games
 		$arr = explode('@',variable_get('app_admin_email',"@$short_league_name"));
-		$domain_url = $arr[1];
+		$this->smarty->assign('domain_url', $arr[1]);
 
-		// ical header
-		print utf8_encode("BEGIN:VCALENDAR
-PRODID:-//Leaguerunner//Team Schedule//EN
-VERSION:2.0
-CALSCALE:GREGORIAN
-METHOD:PUBLISH
-X-WR-CALNAME:$my_team schedule from $short_league_name
-");
-
-		// TODO: add VTIMEZONE group
-
+		// date stamp this file
+		// MUST be in UTC
+		$this->smarty->assign('now', gmstrftime('%Y%m%dT%H%M%SZ'));
 
 		while(list(,$game) = each($games)) {
-			if($game->home_id == $this->team->team_id) {
-				$opponent_id = $game->away_id;
-				$opponent_name = $game->away_name;
-				$home_away = '(home)';
-			} else {
-				$opponent_id = $game->home_id;
-				$opponent_name = $game->home_name;
-				$home_away = '(away)';
-			}
+			$opponent_id = ($game->home_id == $this->team->team_id) ? $game->away_id : $game->home_id;
+			$game->opponent = team_load( array('team_id' => $opponent_id) );
 
-			if ($opponent_name == "") {
-				$opponent_name = "(to be determined)";
-				$opponent_colour = "";
-			} else {
-				// look up opponent's shirt colour
-				$opponent_team = team_load( array('team_id' => $opponent_id) );
-				$opponent_colour = $opponent_team->shirt_colour;
-			}
-
-			// encode game start and end times
-			$game_date = strftime('%Y%m%d', $game->timestamp); // from date type
-			$game_start = $game_date . 'T'
-			  . join(explode(':', $game->game_start)) // from 'hh:mm' string
-			  . '00';
-			$game_end = $game_date . 'T'
-					. join(explode(':', $game->display_game_end()))  // from 'hh:mm' string
-					. '00';
-
-			// date stamp this file
-			$now = gmstrftime('%Y%m%dT%H%M%SZ'); // MUST be in UTC
-
-			// generate field url
-			$field_url = url("field/view/$game->fid");
-
-			// look up field's full name
-			$field = field_load(array('fid' => $game->fid));
-
-			// output game
-			// TODO: need to track when games are created/modified
-			// TODO: possible bug; Google Calendar tries to
-			// generate a Google Maps link from the data in
-			// LOCATION, which will always be wrong.  Is this
-			// Google being stupid, or are we violating the spec by
-			// not using something address-ish in that field
-			// TODO: What's with the X- fields?
-			print utf8_encode("BEGIN:VEVENT
-UID:$game->game_id@$domain_url
-DTSTAMP:$now
-CREATED:20090101T000000Z
-LAST-MODIFIED:20090101T000000Z
-DTSTART;$timezone:$game_start
-DTEND;$timezone:$game_end
-LOCATION:$field->fullname ($game->field_code)
-X-LOCATION-URL:$field_url
-SUMMARY:$my_team vs. $opponent_name
-DESCRIPTION:Game $game->game_id: $my_team vs. $opponent_name at $field->fullname ($game->field_code) on ".strftime('%a %b %d %Y', $game->timestamp)." $game->game_start to " . $game->display_game_end()
-. ($opponent_colour ? " (they wear $opponent_colour)" : "") . "
-X-OPPONENT-COLOUR:$opponent_colour
-STATUS:CONFIRMED
-TRANSP:OPAQUE
-END:VEVENT
-");
-
+			$game->field = field_load(array('fid' => $game->fid));
 		}
 
-		print "END:VCALENDAR\n";
-		exit; // don't return, as we don't want the HTML printed
+		$this->smarty->assign('games', $games);
 	}
 }
 ?>
---
+++ templates/pages/team/ical.tpl
@@ -0,0 +1,39 @@
+BEGIN:VCALENDAR
+PRODID:-//Leaguerunner//Team Schedule//EN
+VERSION:2.0
+CALSCALE:GREGORIAN
+METHOD:PUBLISH
+X-WR-CALNAME:{$team->name|utf8} schedule from {$short_league_name}
+{* TODO: add VTIMEZONE group *}
+
+{foreach from=$games item=game}
+BEGIN:VEVENT
+UID:{$game->game_id}@{$domain_url}
+DTSTAMP:{$now}
+{* TODO: created and last-modified are bullshit *}
+CREATED:20090101T000000Z
+LAST-MODIFIED:20090101T000000Z
+{* TODO: do we need tzid in here? *}
+DTSTART;TZID={$timezone}:{$game->timestamp|date_format:"%Y%m%d"}T{$game->game_start|replace:':':''}00
+DTSTART;TZID={$timezone}:{$game->timestamp|date_format:"%Y%m%d"}T{$game->game_end|replace:':':''}00
+{*
+	TODO: possible bug; Google Calendar tries to generate a Google Maps
+	link from the data in LOCATION, which will always be wrong.  Is this
+	Google being stupid, or are we violating the spec by not using
+	something address-ish in that field
+
+	TODO: What's with the X- fields?
+*}
+LOCATION:{$game->field->fullname} ({$game->field_code})
+X-LOCATION-URL:{lr_url path="field/view/`$game->fid`}
+SUMMARY:{$game->home_name|utf8} (home) vs. {$game->away_name|utf8} (away)
+DESCRIPTION:Game {$game->game_id}: {$game->home_name|utf8} (home) vs. {$game->away_name|utf8} (away) at {$game->field->fullname} ({$game->field_code}) on {$game->timestamp|date_format:"%a %b %d %Y"} {$game->game_start} to {$game->display_game_end()}{if $game->opponent->shirt_colour}(they wear {$game->opponent->shirt_colour|utf8})
+{else}
+{/if}
+X-OPPONENT-COLOUR: {$game->opponent->shirt_colour|utf8}
+STATUS:CONFIRMED
+TRANSP:OPAQUE
+END:VEVENT
+{/foreach}
+
+END:VCALENDAR
