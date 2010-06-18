26d2ded69a70d9736e61af642cf093ae0dafd1a2
Dave O'Neill <dmo@dmo.ca>
2010-06-18 00:10:15 -0400

    Templating: team/view now templated, and uses jQuery for table sort

--- src/Handler/team/view.php
+++ src/Handler/team/view.php
@@ -13,106 +13,37 @@ class team_view extends TeamHandler
 	{
 		global $lr_session, $dbh;
 
-		// Team names might have HTML in them, so we need to nuke it.
-		$team_name = check_form($this->team->name, ENT_NOQUOTES);
 		$this->title = $team_name;
 
-		// Now build up team data
-		$rows = array();
-		if($this->team->website) {
-			$rows[] = array("Website:", l($this->team->website, $this->team->website));
-		}
-		$rows[] = array("Shirt Colour:", check_form($this->team->shirt_colour, ENT_NOQUOTES));
-		$rows[] = array("League/Tier:", l($this->team->league_name, "league/view/" . $this->team->league_id));
+		$this->template_name = 'pages/team/view.tpl';
+		$this->smarty->assign('team', $this->team);
 
 		if($this->team->home_field) {
 			$field = field_load(array('fid' => $this->team->home_field));
-			$rows[] = array("Home Field:", l($field->fullname,"field/view/$field->fid"));
+			$this->smarty->assign('home_field', $field);
 		}
 
-		if($this->team->region_preference) {
-			$rows[] = array("Region preference:", $this->team->region_preference);
-		}
-
-		$rows[] = array("Team Status:", $this->team->status);
-
-		/* Spence Balancing Factor:
-		 * Average of all score differentials.  Lower SBF means more
-		 * evenly-matched games.
-		 */
 		$teamSBF = $this->team->calculate_sbf( );
 		if( $teamSBF ) {
+			$this->smarty->assign('team_sbf', $teamSBF);
 			$league = league_load( array('league_id' => $this->team->league_id) );
-			$leagueSBF = $league->calculate_sbf();
-			if( $leagueSBF ) {
-				$teamSBF .= " (league $leagueSBF)";
-			}
-			$rows[] = array("Team SBF:", $teamSBF);
+			$this->smarty->assign('league_sbf', $league->calculate_sbf());
 		}
-		$rows[] = array("Rating:", $this->team->rating);
 
-		$teamdata = "<div class='pairtable'>" . table(null, $rows) . "</div>";
-
-		$header = array( 'Name', 'Position', 'Gender','Rating' );
 		if( $lr_session->has_permission('team','player shirts', $this->team->team_id) ) {
-			array_push($header, 'Shirt Size');
+			$this->smarty->assign('display_shirts', true);
 		}
-		array_push($header, 'Date Joined');
-		$rows = array();
-		$totalSkill = 0;
-		$skillCount = 0;
 		$rosterCount = 0;
 		$rosterPositions = getRosterPositions();
+		$this->smarty->assign('roster_positions', $rosterPositions);
 
 		$this->team->get_roster();
+		$this->team->check_roster_conflict();
 		foreach ($this->team->roster as $player) {
-			/*
-			 * Now check for conflicts.  Players who are subs get
-			 * conflicts ignored, but not others.
-			 *
-			 * TODO: This is time-consuming and resource-inefficient.
-			 * TODO: Turn this into $team->check_roster_conflicts()
-			 */
-			$c_sth = $dbh->prepare("SELECT COUNT(*) from
-					league l, leagueteams t, teamroster r
-				WHERE
-					l.year = ? AND l.season = ? AND l.day = ?
-					AND r.status != 'substitute'
-					AND l.schedule_type != 'none'
-					AND l.league_id = t.league_id
-					AND l.status = 'open'
-					AND t.team_id = r.team_id
-					AND r.player_id = ?");
-			$c_sth->execute(array(
-				$this->team->league_year,
-				$this->team->league_season,
-				$this->team->league_day,
-				$player->id
-			));
-
-			if($c_sth->fetchColumn() > 1) {
-				$conflictText = "(roster conflict)";
-			} else {
-				$conflictText = null;
-			}
-
-			if($player->player_status == "inactive" ) {
-				if($conflictText) {
-					$conflictText .= "<br />(account inactive)";
-				} else {
-					$conflictText .= "(account inactive)";
-				}
-			}
-
-			$player_name = l($player->fullname, "person/view/$player->id");
-			if( $conflictText ) {
-				$player_name .= "<div class='roster_conflict'>$conflictText</div>";
-			}
-
 			if($lr_session->has_permission('team','player status', $this->team->team_id, $player->id) ) {
-				$roster_info = l($rosterPositions[$player->status], "team/roster/" . $this->team->team_id . "/$player->id");
+				$player->_modify_status = 1;
 			} else {
-				$roster_info = $rosterPositions[$player->status];
+				$player->_modify_status = 0;
 			}
 			if( $player->status == 'captain' ||
 				$player->status == 'assistant' ||
@@ -120,43 +51,7 @@ class team_view extends TeamHandler
 			) {
 				++$rosterCount;
 			}
-
-			$row = array(
-				$player_name,
-				$roster_info,
-				$player->gender,
-				$player->skill_level
-			);
-			if( $lr_session->has_permission('team','player shirts', $this->team->team_id) ) {
-				array_push($row, $player->shirtsize);
-			}
-			array_push($row, $player->date_joined);
-			$rows[] = $row;
-
-			$totalSkill += $player->skill_level;
-			if ($player->skill_level) {
-				$skillCount ++;
-			}
-		}
-
-		if($skillCount > 0) {
-			$avgSkill = sprintf("%.2f", ($totalSkill / $skillCount));
-		} else {
-			$avgSkill = 'N/A';
-		}
-		$rows[] = array(
-			array('data' => 'Average Skill Rating', 'colspan' => 3),
-			$avgSkill
-		);
-
-		$rosterdata = "<div class='listtable'>" . table($header, $rows) . "</div>";
-
-		if( variable_get('narrow_display', '0') ) {
-			$rc = $teamdata . '<p />' . $rosterdata;
-		} else {
-			$rc = table(null, array(
-				array( $teamdata, $rosterdata ),
-			));
+			$player->status = $rosterPositions[$player->status];
 		}
 
 		if( $rosterCount < 12 && $lr_session->is_captain_of($this->team->team_id) && $this->team->roster_deadline > 0 ) {
---
+++ templates/pages/team/view.tpl
@@ -0,0 +1,101 @@
+{include file=header.tpl}
+<h1>{$title}</h1>
+<table>
+  <tr><td>
+  <div class='pairtable'><table>
+    {if $team->website}<tr><td>Website:</td><td><a href="{$team->website|escape}">{$team->website|escape}</a></td></tr>{/if}
+    <tr><td>Shirt Colour:</td><td>{$team->shirt_colour|escape}</td></tr>
+    <tr><td>League/Tier:</td><td><a href="{lr_url path="league/view/`$team->league_id`"}">{$team->league_name|escape}</a></td></tr>
+    {if $home_field}
+	<tr><td>Home Field:</td><td><a href="{lr_url path="field/view/`$home_field->fid`"}">{$home_field->fullname|escape}</a></td></tr>
+    {/if}
+    <tr><td>Region Preference:</td><td>{$team->region_preference|default:"None"}</td></tr>
+    <tr><td>Team Status:</td><td>{$team->status}</td></tr>
+
+    <tr><td>Team SBF:</td><td>{$team_sbf} {if $league_sbf}(league {$league_sbf}){/if}</td></tr>
+    <tr><td>Rating:</td><td>{$team->rating}</td></tr>
+  </table></div>
+  </td><td>
+    <table id="roster">
+       <thead>
+	<tr><th>Name</th><th>Position</th><th>Gender</th><th>Rating</th>{if $display_shirts}<th>Shirt Size</th>{/if}<th>Date Joined</th></tr>
+       </thead>
+       <tbody>
+       {foreach from=$team->roster item=p}
+		<tr>
+		  <td><a href="{lr_url path="person/view/`$p->id`"}">{$p->fullname}</a>
+		    {if $p->roster_conflict}<div class='roster_conflict'>(roster conflict)</div>{/if}
+		    {if $p->player_status == "inactive"}<div class='roster_conflict'>(account inactive)</div>{/if}
+		  </td>
+		  <td>{if $p->_modify_status}<a href="{lr_url path="team/roster/`$team->team_id`/`$p->id`"}">{$p->status}</a>{else}{$p->status}{/if}</td>
+		  <td>{$p->gender}</td>
+		  <td>{$p->skill_level}</td>
+		  {if $display_shirts}<td>{$p->shirtsize}</td>{/if}
+		  <td>{$p->date_joined}</td>
+		</tr>
+       {/foreach}
+	</tbody>
+	<tfoot>
+	<tr><th colspan="3">Average Skill Rating</th><th></th></tr>
+	</tfoot>
+</table>
+</td></tr>
+</table>
+<script type="text/javascript">
+{literal}
+var positionSort = {
+{/literal}
+	'{$roster_positions.captain}'         : 1,
+	'{$roster_positions.assistant}'       : 2,
+	'{$roster_positions.player}'          : 3,
+	'{$roster_positions.substitute}'      : 4,
+	'{$roster_positions.player_request}'  : 5,
+	'{$roster_positions.captain_request}' : 6,
+	'{$roster_positions.coach}'           : 0,
+{literal}
+};
+jQuery.fn.dataTableExt.oSort['roster-position-desc']  = function(a,b) {
+	var x = positionSort[ a.replace(/\n/g," ").replace( /<.*?>/g, "" )+'' ];
+	var y = positionSort[ b.replace(/\n/g," ").replace( /<.*?>/g, "" )+'' ];
+	return ((x < y) ? -1 : ((x > y) ?  1 : 0));
+};
+
+jQuery.fn.dataTableExt.oSort['roster-position-asc']  = function(a,b) {
+	var x = positionSort[a.replace(/\n/g," ").replace( /<.*?>/g, "" )+''];
+	var y = positionSort[b.replace(/\n/g," ").replace( /<.*?>/g, "" )+''];
+	return ((x < y) ? 1 : ((x > y) ?  -1 : 0));
+};
+$(document).ready(function() {
+	$('#roster').dataTable( {
+		bPaginate: false,
+		bFilter: false,
+		bInfo: false,
+		aaSorting: [[ 1, "desc" ],[2,"desc"], [0, "asc"] ],
+		aoColumns: [
+			{ "sType" : "html" },
+			{ "sType" : "roster-position",  },
+			null,
+			null,
+{/literal}{if $display_shirts}
+			null,
+{/if}{literal}
+			{ "sType" : "string" },
+		],
+		"fnFooterCallback": function ( nRow, aaData, iStart, iEnd, aiDisplay ) {
+			var totalskill = 0;
+			var numplayers = 0;
+			for ( var i=0 ; i<aaData.length ; i++ )
+			{
+				totalskill += aaData[i][3]*1;
+				numplayers++;
+			}
+
+			var nCells = nRow.getElementsByTagName('th');
+			nCells[1].innerHTML = (totalskill/numplayers).toFixed(2);
+		}
+	} );
+	$('#roster tr:even').addClass('even');
+})
+{/literal}
+</script>
+{include file=footer.tpl}
