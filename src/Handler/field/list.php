5c0bbaa658d5f64cd7ca8c300fe2d15452b41716
Dave O'Neill <dmo@dmo.ca>
2010-06-18 23:56:34 -0400

    Templating: field/list.
    Also, removes separate view for closed fields.  Users with access to see them
    will now see them listed on the same list, but visually differentiated.

--- src/Handler/field/list.php
+++ src/Handler/field/list.php
@@ -2,75 +2,39 @@
 
 class field_list extends Handler
 {
-	function __construct ( $type = null )
-	{
-		$this->closed = ( isset( $type ) && $type == 'closed' );
-	}
 
 	function has_permission()
 	{
 		global $lr_session;
-		return $lr_session->has_permission('field','list',$this->closed);
+		return $lr_session->has_permission('field','list');
 	}
 
 	function process ()
 	{
-		global $CONFIG;
+		global $lr_session;
 
-		$output = '';
-		if( $this->closed ) {
-			$this->title = 'List Closed Fields';
-		} else {
-			$this->title = 'List Fields';
+		$this->template_name = 'pages/field/list.tpl';
 
-			ob_start();
-			$retval = @readfile(trim ($CONFIG['paths']['file_path'], '/') . "/data/field_caution.html");
-			if (false !== $retval) {
-				$output .= ob_get_contents();
-			}
-			ob_end_clean();
-		}
+		$this->title = 'List Fields';
 
-		// TODO: this open/closed crap doesn't work everywhere!
-		if( $this->closed ) {
-			$status = "AND status = 'closed'";
+		if( $lr_session->has_permission('field','list','closed') ) {
+			$status = '';
 		} else {
 			$status = "AND (status = 'open' OR ISNULL(status))";
 		}
 		$sth = field_query( array( '_extra' => "ISNULL(parent_fid) $status", '_order' => 'f.region,f.name') );
 
-		$fieldsByRegion = array();
+		$fields = array();
 		while($field = $sth->fetch(PDO::FETCH_OBJ) ) {
-			if(! array_key_exists( $field->region, $fieldsByRegion) ) {
-				$fieldsByRegion[$field->region] = array();
+			if(! array_key_exists( $field->region, $fields) ) {
+				$fields[$field->region] = array();
 			}
-			array_push( $fieldsByRegion[$field->region], array( l($field->name, "field/view/$field->fid") ));
+			array_push( $fields[$field->region], $field );
 		}
 
-		$fieldColumns = array();
-		$header = array();
+		$this->smarty->assign('fields_by_region', $fields);
 
-		if( variable_get('narrow_display', '0') ) {
-			$cols = 2;
-			for( $i = 0; $i < $cols; ++ $i ) {
-				$fieldColumns[$i] = '';
-			}
-
-			$i = 0;
-			while(list($region,$fields) = each($fieldsByRegion)) {
-				$fieldColumns[$i] .= table( array( ucfirst( $region ) ), array( array( table(null, $fields) ) ) );
-				$i = ( $i + 1 ) % $cols;
-			}
-		}
-		else {
-			while(list($region,$fields) = each($fieldsByRegion)) {
-				$fieldColumns[] = table( null, $fields) ;
-				$header[] = ucfirst($region);
-			}
-		}
-		$output .= "<div class='fieldlist'>" . table($header, array( $fieldColumns) ) . "</div>";
-
-		return $output;
+		return true;
 	}
 }
 ?>
--- templates/pages/field/list.tpl
+++ templates/pages/field/list.tpl
@@ -1,6 +1,9 @@
-<p>Please respect the following rules for all fields.  Note that some sites also
-have additional restrictions listed on that field's page that must also be
-followed.</p>
+{include file=header.tpl}
+<h1>{$title}</h1>
+<p>
+	Please respect the following rules for all fields.  Note that some
+	sites also have additional restrictions listed on that field's page
+	that must also be followed.</p>
 <ul>
   <li>Garbage facilites do not exist at any site.  Do not leave any garbage
       behind when you leave -- even if it isn't yours.  Take extra care to
@@ -21,3 +24,18 @@ teams ignoring the rules, we are close to losing several of our fields.  If
 fields are lost due to the actions of a particular player or team, they will
 be <b>removed from the league</b>.
 </p>
+<div class="fieldlist">
+<table>
+{foreach from=$fields_by_region key=region item=fields}
+<tr>
+  <th>{$region|capitalize}</th>
+  <td>
+     {foreach from=$fields item=f}
+     <a {if $f->status == 'closed'}class='closedfield'{/if} href="{lr_url path="field/view/`$f->fid`"}">{$f->name}</a>,&nbsp;
+     {/foreach}
+  </td>
+</tr>
+{/foreach}
+</table>
+</div>
+{include file=footer.tpl}
