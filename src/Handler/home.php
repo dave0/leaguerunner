ec8b5e90d6ebcb09a7c32ab0161386aad783fbc5
Dave O'Neill <dmo@dmo.ca>
2010-07-16 00:06:32 -0400

    Templating: home

--- src/Handler/home.php
+++ src/Handler/home.php
@@ -10,227 +10,122 @@ class home extends Handler
 
 	function process ()
 	{
-		global $lr_session;
+		global $lr_session, $dbh;
+
 		$this->title =  $lr_session->attr_get('fullname');
-		return "<div class='splash'>"
-			. home_splash()
-			. team_splash()
-			. league_splash()
-			. game_splash()
-			. "</div>";
-	}
-}
+		$this->template_name = 'pages/home.tpl';
 
-function home_splash ()
-{
-	# Init curl
-	$curl = curl_init();
-	curl_setopt($curl, CURLOPT_URL, variable_get('rss_feed_url', 'http://www.ocua.ca/taxonomy/term/140/all/feed') );
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 0);
+		/* Handle display of RSS feed itmes, if enabled */
+		$feed_url =  variable_get('rss_feed_url', null);
+		if( $feed_url ) {
+			$curl = curl_init();
+			curl_setopt($curl, CURLOPT_URL, $feed_url);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 0);
 
-	# Fetch RSS data
-	$xml = curl_exec($curl);
-	curl_close($curl);
+			# Fetch RSS data
+			$xml = curl_exec($curl);
+			curl_close($curl);
 
-	$xmlObj = simplexml_load_string( $xml );
+			$xmlObj = simplexml_load_string( $xml );
 
-	$count = 0;
-	$limit = variable_get('rss_feed_items', 2);
-	$rows = array();
+			$count = 0;
+			$limit = variable_get('rss_feed_items', 2);
+			$items = array();
 
-	foreach ( $xmlObj->channel[0]->item as $item )
-	{
-		$rows[] = array(
-			l($item->title, $item->link)
-		);
-		if( ++$count >= $limit ) {
-			break;
+			foreach ( $xmlObj->channel[0]->item as $item )
+			{
+				$items[] = array(
+					'title' => $item->title,
+					'link'  => $item->link,
+				);
+				if( ++$count >= $limit ) {
+					break;
+				}
+			}
+			if( $count > 0 ) {
+				$this->smarty->assign('rss_feed_title', variable_get('rss_feed_title', 'OCUA Volunteer Opportunities'));
+				$this->smarty->assign('rss_feed_items', $items);
+			}
 		}
-	}
-	if( $count > 0 ) {
-		return table( array( array('data' => variable_get('rss_feed_title', 'OCUA Volunteer Opportunities'), 'colspan' => 2),), $rows);
-	} else {
-		return '';
-	}
-}
 
-/**
- * Generate view of teams for initial login splash page.
- */
-function team_splash ()
-{
-	global $lr_session;
-	$rows = array();
-	$rows[] = array('','', array( 'data' => '','width' => 90), '');
+		/* Display teams */
+		$rosterPositions = getRosterPositions();
+		$teams = array();
+		foreach($lr_session->user->teams as $team) {
+			$team->position = $rosterPositions[$team->position];
+			$teams[] = $team;
+		}
+		reset($lr_session->user->teams);
+		$this->smarty->assign('teams', $teams);
+	/*
+	* 	TODO nuke historical_teams
+		if( count($lr_session->user->historical_teams) ) {
+			$rows[] = array( array('colspan' => 2, 'data' => 'You have ' . l('historical team data', "person/historical/{$lr_session->user->user_id}") . ' saved'));
+		}
+	*/
 
-	$rosterPositions = getRosterPositions();
-	$rows = array();
-	foreach($lr_session->user->teams as $team) {
-		$position = $rosterPositions[$team->position];
+		/* Display leagues */
+		// TODO: For each league, need to display # of missing scores,
+		// pending scores, etc.
+		$this->smarty->assign('leagues', $lr_session->user->leagues);
 
-		$rows[] =
-			array(
-				l($team->name, "team/view/$team->id") . " ($team->position)",
-				array('data' => theme_links(array(
-						l("schedule", "team/schedule/$team->id"),
-                  l("standings", "league/standings/$team->league_id/$team->team_id"))),
-					  'align' => 'right')
-		);
+		/* Display recent and upcoming games */
+		$games = array();
+		// TODO: query should be moved to person object, or a helper in game.inc
+		$sth = $dbh->prepare('SELECT s.game_id, t.team_id, t.status FROM schedule s, gameslot g, teamroster t WHERE s.published AND ((s.home_team = t.team_id OR s.away_team = t.team_id) AND t.player_id = ?) AND g.game_id = s.game_id AND g.game_date < CURDATE() ORDER BY g.game_date desc, g.game_start desc LIMIT 4');
+		$sth->execute( array($lr_session->user->user_id) );
 
-	}
-	reset($lr_session->user->teams);
-	if( count($lr_session->user->teams) < 1) {
-		$rows[] = array( array('colspan' => 2, 'data' => 'You are not yet on any teams'));
-	}
-	if( count($lr_session->user->historical_teams) ) {
-		$rows[] = array( array('colspan' => 2, 'data' => 'You have ' . l('historical team data', "person/historical/{$lr_session->user->user_id}") . ' saved'));
-	}
-	return table( array( array('data' => 'My Teams', 'colspan' => 2),), $rows);
-}
+		while($row = $sth->fetch(PDO::FETCH_OBJ) ) {
+			$game = game_load(array('game_id' => $row->game_id));
+			$game->user_team_id = $row->team_id;
 
-/**
- * Generate view of leagues for initial login splash page.
- */
-function league_splash ()
-{
-	global $lr_session;
-	if( ! $lr_session->user->is_a_coordinator ) {
+			$games[] = $game;
+		}
+		$games = array_reverse($games);
+
+		$sth = $dbh->prepare('SELECT s.game_id, t.team_id, t.status FROM schedule s, gameslot g, teamroster t WHERE s.published AND ((s.home_team = t.team_id OR s.away_team = t.team_id) AND t.player_id = ?) AND g.game_id = s.game_id AND g.game_date >= CURDATE() ORDER BY g.game_date asc, g.game_start asc LIMIT 4');
+		$sth->execute( array($lr_session->user->user_id) );
+
+		while($row = $sth->fetch(PDO::FETCH_OBJ) ) {
+			$game = game_load(array('game_id' => $row->game_id));
+			$game->user_team_id = $row->team_id;
+			$games[] = $game;
+		}
+
+		foreach($games as $game) {
+			$score = '';
+			if( $game->is_finalized() ) {
+				$score = "$game->home_score - $game->away_score"	;
+			} else {
+				/* Not finalized yet, so we will either:
+				*   - display entered score if present
+				*   - display score entry link if game date has passed
+				*   - display a blank otherwise
+				*/
+				$entered = $game->get_score_entry( $row->team_id );
+				if($entered) {
+					// need to match entered score order to displayed team order!
+					if ($entered->team_id == $game->home_id) {
+						$score = "$entered->score_for - $entered->score_against";
+					} else {
+						$score = "$entered->score_against - $entered->score_for";
+					}
+					$score .= " (unofficial, waiting for opponent)";
+					$game->score_entered = $score;
+
+				} else if($lr_session->has_permission('game','submit score', $game)
+					&& ($game->timestamp < time()) ) {
+					$game->user_can_submit = true;
+				}
+			}
+
+		}
+
+		$this->smarty->assign('games', $games);
 		return;
 	}
 
-	$header = array(
-		array( 'data' => "Leagues Coordinated", 'colspan' => 4)
-	);
-	$rows = array();
-
-	// TODO: For each league, need to display # of missing scores,
-	// pending scores, etc.
-	while(list(,$league) = each($lr_session->user->leagues)) {
-		$links = array(
-			l("edit", "league/edit/$league->league_id")
-		);
-		if($league->schedule_type != 'none') {
-			$links[] = l("schedule", "schedule/view/$league->league_id");
-			$links[] = l("standings", "league/standings/$league->league_id");
-			$links[] = l("approve scores", "league/approvescores/$league->league_id");
-		}
-
-		$rows[] = array(
-			array(
-				'data' => l($league->fullname, "league/view/$league->league_id"),
-				'colspan' => 3
-			),
-			array(
-				'data' => theme_links($links),
-				'align' => 'right'
-			)
-		);
-	}
-	reset($lr_session->user->leagues);
-
-	return table( $header, $rows );
-}
-
-/**
- * Generate view of games for initial login splash page.
- */
-function game_splash ()
-{
-	global $lr_session, $dbh;
-
-	$sth = game_load(array());
-	$sth = $dbh->prepare('SELECT s.game_id, t.team_id, t.status FROM schedule s, gameslot g, teamroster t WHERE s.published AND ((s.home_team = t.team_id OR s.away_team = t.team_id) AND t.player_id = ?) AND g.game_id = s.game_id AND g.game_date < CURDATE() ORDER BY g.game_date desc, g.game_start desc LIMIT 4');
-	$sth->execute( array($lr_session->user->user_id) );
-
-	$rows = array();
-	while($row = $sth->fetch(PDO::FETCH_OBJ) ) {
-		$game = game_load(array('game_id' => $row->game_id));
-		$score = 'not entered';
-		if( $game->is_finalized() ) {
-			$score = "$game->home_score - $game->away_score"	;
-		} else {
-			/* Not finalized yet, so we will either:
-			 *   - display entered score if present
-			 *   - display score entry link if game date has passed
-			 *   - display a blank otherwise
-			 */
-			$entered = $game->get_score_entry( $row->team_id );
-			if($entered) {
-				// need to match entered score order to displayed team order!
-				if ($entered->team_id == $game->home_id) {
-					$score = "$entered->score_for - $entered->score_against";
-				} else {
-					$score = "$entered->score_against - $entered->score_for";
-				}
-				$score .= " (unofficial, waiting for opponent)";
-			} else if($lr_session->has_permission('game','submit score', $game)
-				&& ($game->timestamp < time()) ) {
-					$score = l("submit score", "game/submitscore/$game->game_id/" . $row->team_id);
-			}
-		}
-		$field = field_load(array('fid' => $game->fid));
-		array_unshift($rows, array(
-			l( strftime('%a %b %d', $game->timestamp) . ", $game->game_start-" . $game->display_game_end(),"game/view/$game->game_id"),
-			array('data' =>
-				l($game->home_name, "team/view/$game->home_id") .
-				" (home) vs. " .
-				l($game->away_name, "team/view/$game->away_id") .
-				" (away) at " .
-				l($game->field_code, "field/view/$game->fid",
-				  array('title' => $field->fullname))),
-			$score
-		));
-	}
-
-	$sth = $dbh->prepare('SELECT s.game_id, t.team_id, t.status FROM schedule s, gameslot g, teamroster t WHERE s.published AND ((s.home_team = t.team_id OR s.away_team = t.team_id) AND t.player_id = ?) AND g.game_id = s.game_id AND g.game_date >= CURDATE() ORDER BY g.game_date asc, g.game_start asc LIMIT 4');
-	$sth->execute( array($lr_session->user->user_id) );
-
-	while($row = $sth->fetch(PDO::FETCH_OBJ) ) {
-		$game = game_load(array('game_id' => $row->game_id));
-		$score = '';
-		if( $game->is_finalized() ) {
-			$score = "$game->home_score - $game->away_score"	;
-		} else {
-			/* Not finalized yet, so we will either:
-			 *   - display entered score if present
-			 *   - display score entry link if game date has passed
-			 *   - display a blank otherwise
-			 */
-			$entered = $game->get_score_entry( $row->team_id );
-			if($entered) {
-				// need to match entered score order to displayed team order!
-				if ($entered->team_id == $game->home_id) {
-					$score = "$entered->score_for - $entered->score_against";
-				} else {
-					$score = "$entered->score_against - $entered->score_for";
-				}
-				$score .= " (unofficial, waiting for opponent)";
-			} else if($lr_session->has_permission('game','submit score', $game)
-				&& ($game->timestamp < time()) ) {
-					$score = l("submit score", "game/submitscore/$game->game_id/" . $row->team_id);
-			}
-		}
-		$field = field_load(array('fid' => $game->fid));
-		$rows[] = array(
-			l( strftime('%a %b %d', $game->timestamp) . ", $game->game_start-" . $game->display_game_end(),"game/view/$game->game_id"),
-			array('data' =>
-				l($game->home_name, "team/view/$game->home_id") .
-				" (home) vs. " .
-				l($game->away_name, "team/view/$game->away_id") .
-				" (away) at " .
-				l($game->field_code, "field/view/$game->fid",
-				  array('title' => $field->fullname))),
-			$score
-		);
-	}
-
-	# If no recent games, don't display the table
-	if( count($rows) < 1)  {
-		return;
-	}
-
-	return "<div class='schedule'>" . table(array( array( 'data' => "Recent and Upcoming Games", 'colspan' => 3)), $rows, array('alternate-colours' => true) ) . "</div>";
 }
 
 ?>
---
+++ templates/pages/home.tpl
@@ -0,0 +1,64 @@
+{include file=header.tpl}
+<h1>{$title}</h1>
+<div class='splash'>
+{if $rss_feed_items}
+<table>
+<tr><th colspan="2">{$rss_feed_title}</th></tr>
+{foreach item=f from=$rss_feed_items}
+<tr><td><a href="{$f.link}">{$f.title}</a></td></tr>
+{/foreach}
+</table>
+{/if}
+
+<table>
+<tr><th colspan="2">My Teams</th></tr>
+{foreach item=t from=$teams}
+<tr><td><a href="{lr_url path="team/view/`$t->team_id`"}">{$t->name}</a> ({$t->position})</td><td align="right"><a href="{lr_url path="team/schedule/`$t->team_id`"}">schedule</a> | <a href="{lr_url path="league/standings/`$t->league_id`/`$t->team_id`"}">standings</a></td></tr>
+{foreachelse}
+<tr><td colspan="2">You are not yet on any teams</td></tr>
+{/foreach}
+</table>
+
+{if $leagues}
+<table>
+<tr><th colspan="2">Leagues</th></tr>
+{foreach item=l from=$leagues}
+<tr>
+  <td><a href="{lr_url path="league/view/`$l->league_id`"}">{$l->fullname}</a></td>
+  <td align="right">
+      <a href="{lr_url path="league/edit/`$l->league_id`"}">edit</a> 
+      {if $l->schedule_type != 'none'}
+      | <a href="{lr_url path="league/schedule/`$l->league_id`"}">schedule</a> 
+      | <a href="{lr_url path="league/standings/`$l->league_id`"}">standings</a>
+      | <a href="{lr_url path="league/approvescores/`$l->league_id`"}">approve scores</a>
+      {/if}
+  </td>
+</tr>
+{/foreach}
+</table>
+{/if}
+
+<div class='schedule'><table alternate-colours="1">
+<tr><th colspan="3">Recent and Upcoming Games</th></tr>
+{foreach item=g from=$games}
+{* TODO: jQuery for alternating row colours *}
+<tr>
+  <td><a href="{lr_url path="game/view/`$g->game_id`"}">{$g->timestamp|date_format:"%a %b %d"}, {$g->game_start}-{$g->display_game_end()}</a></td>
+  <td><a href="{lr_url path="team/view/`$g->home_id`"}">{$g->home_name}</a> (home) vs. <a href="{lr_url path="team/view/`$g->away_id`"}">{$g->away_name}</a> (away) at <a href="{lr_url path="field/view/`$g->fid`}">{$g->field_code}</a></td>
+  <td>{if $g->is_finalized()}
+  	{$g->home_score} - {$g->away_score}
+  {else}
+  	{if $g->score_entered}
+		{$g->score_entered}
+	{elseif $g->user_can_submit}
+		<a href="{lr_url path="game/submitscore/`$g->game_id`/`$g->user_team_id`"}">submit score</a>
+	{/if}
+  {/if}
+  </td>
+</tr>
+{foreachelse}
+{/foreach}
+</table>
+</div></div>
+
+{include file=footer.tpl}
