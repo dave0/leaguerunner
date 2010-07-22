9895c93cf1a2d54f7ac31936ad9dbbf7c9a4f189
Dave O'Neill <dmo@dmo.ca>
2010-07-22 01:48:35 -0400

    Templating: league/status
    Also refactors a lot of the code to be substantially less stupid.  No more
    iterating over the entire schedule N times, for example!

--- src/Handler/league/status.php
+++ src/Handler/league/status.php
@@ -12,16 +12,12 @@ class league_status extends LeagueHandler
 	{
 		$this->title = "{$this->league->fullname} &raquo; Status Report";
 
-		$rc = $this->generateStatusPage();
+		$this->template_name = 'pages/league/status.tpl';
 
-		return $rc;
-	}
-
-	function generateStatusPage ( )
-	{
 		// make sure the teams are loaded
 		$this->league->load_teams();
 
+		// TODO: we calculate_standings() here, but it's probably not necessary
 		list($order, $season, $round) = $this->league->calculate_standings(array( 'round' => $this->league->current_round ));
 
 		$fields = array();
@@ -30,140 +26,67 @@ class league_status extends LeagueHandler
 			$fields[$field->code] = $field->region;
 		}
 
-		$output = para("This is a general scheduling status report for rating ladder leagues.");
-
-		$header[] = array('data' => "Rating", 'rowspan' => 2);
-		$header[] = array('data' => "Team", 'rowspan' => 2);
-		$header[] = array('data' => "Games", 'rowspan' => 2);
-		$header[] = array('data' => "Home/Away", 'rowspan' => 2);
-		$header[] = array('data' => "Region", 'colspan' => 4);
-		$header[] = array('data' => "Region Pct", 'rowspan' => 2);
-		$header[] = array('data' => "Opponents", 'rowspan' => 2);
-		$header[] = array('data' => "Repeat Opponents", 'rowspan' => 2);
-
-		$subheader[] = array('data' => "C", 'class' => "subtitle");
-		$subheader[] = array('data' => "E", 'class' => "subtitle");
-		$subheader[] = array('data' => "S", 'class' => "subtitle");
-		$subheader[] = array('data' => "W", 'class' => "subtitle");
-
-		$rows = array();
-		$rows[] = $subheader;
-
-		$rowstyle = "standings_light";
-
-		// get the schedule
-		$schedule = array();
+		// Parse the schedule and accumulate per-team stats
 		$sth = game_query ( array( 'league_id' => $this->league->league_id, '_order' => 'g.game_date, g.game_start, field_code') );
 		while($g = $sth->fetchObject('Game') ) {
-			$schedule[] = $g;
+
+			$season[$g->home_team]->game_count++;
+			$season[$g->away_team]->game_count++;
+
+			list($code, $num) = explode(' ', $g->field_code);
+			$season[$g->home_team]->region_game_counts[$fields[$code]]++;
+			$season[$g->away_team]->region_game_counts[$fields[$code]]++;
+
+			$season[$g->home_team]->home_game_count++;
+
+			$season[$g->home_team]->opponent_counts[$g->away_name]++;
+			$season[$g->away_team]->opponent_counts[$g->home_name]++;
 		}
 
+		$teams = array();
 		while(list(, $tid) = each($order)) {
-			if ($rowstyle == "standings_light") {
-				$rowstyle = "standings_dark";
-			} else {
-				$rowstyle = "standings_light";
-			}
-			$row = array( array('data'=>$season[$tid]->rating, 'class'=>"$rowstyle") );
-			$row[] = array('data'=>l($season[$tid]->name, "team/view/$tid"), 'class'=>"$rowstyle");
+			$team = $season[$tid];
 
-			// count number of games for this team:
-			//$games = game_load_many( array( 'either_team' => $this->team->team_id, '_order' => 'g.game_date,g.game_id') );
-			$numgames = 0;
-			$homegames = 0;
-			$awaygames = 0;
-
-			$region = array(
-				'Central' => 0,
-				'East' => 0,
-				'South' => 0,
-				'West' => 0,
-			);
-
-			$opponents = array();
-
-			// parse the schedule
-			reset($schedule);
-			while(list(,$game) = each($schedule)) {
-				if ($game->home_team == $tid) {
-					$numgames++;
-					$homegames++;
-					$opponents[$game->away_team]++;
-				}
-				if ($game->away_team == $tid) {
-					$numgames++;
-					$awaygames++;
-					$opponents[$game->home_team]++;
-				}
-				if ($game->home_team == $tid || $game->away_team == $tid) {
-					list($code, $num) = explode(' ', $game->field_code);
-					$region[$fields[$code]]++;
-				}
-			}
-			//reset($games);
-
-			$row[] = array('data'=>$numgames, 'class'=>"$rowstyle", 'align'=>"center");
-			$row[] = array('data'=> _ratio_helper( $homegames, $numgames), 'class'=>$rowstyle, 'align'=>"center");
-
-			// regions:
-			$pref = '---';
-			$region_count = 0;
-			if ($season[$tid]->region_preference != "---" && $season[$tid]->region_preference != "") {
-				$pref = $season[$tid]->region_preference;
-				$region_count  = $region[$pref];
-				$region[$pref] = "<b><font color='blue'>$region_count</font></b>";
+			// region preference
+			if ($team->region_preference != "---" && $team->region_preference != "") {
+				list($team->in_region_ratio, $team->in_region_ratio_bad) = _ratio_helper( $team->region_game_counts[$team->region_preference], $team->game_count);
 			} else {
 				// No region preference means they're always happy :)
-				$region_count = $numgames;
+				$team->in_region_ratio = '1.000';
+				$team->in_region_ratio_bad = false;
 			}
-			$row[] = array('data'=>$region['Central'], 'class'=>"$rowstyle");
-			$row[] = array('data'=>$region['East'], 'class'=>"$rowstyle");
-			$row[] = array('data'=>$region['South'], 'class'=>"$rowstyle");
-			$row[] = array('data'=>$region['West'], 'class'=>"$rowstyle");
-			$row[] = array('data'=> _ratio_helper( $region_count, $numgames), 'class' => $rowstyle);
 
-			$row[] = array('data'=>count($opponents), 'class'=>"$rowstyle", 'align'=>"center");
+			list($team->home_game_ratio, $team->home_game_ratio_bad) = _ratio_helper( $team->home_game_count, $team->game_count);
 
-			// figure out the opponent repeats
-			$opponent_repeats="";
-			while(list($oid, $repeats) = each($opponents)) {
-				if ($repeats > 2) {
-					$opponent_repeats .= $season[$oid]->name . " (<font color='red'><b>$repeats</b></font>) <br>";
-				} else if ($repeats > 1) {
-					$opponent_repeats .= $season[$oid]->name . " (<b>$repeats</b>) <br>";
-				}
-			}
-			$row[] = array('data'=>$opponent_repeats, 'class'=>"$rowstyle");
-
-			$rows[] = $row;
+			$teams[] = $team;
 		}
 
-		//$output .= table($header, $rows);
-		$output .= "<div class='listtable'>" . table($header, $rows) . "</div>";
+		$this->smarty->assign('teams', $teams);
 
-		return form($output);
+		return true;
 	}
 }
 
+/*
+ * Calculate ratio to 3 decimal places, and flag if we're below 0.500.
+ *
+ * For an odd value of $total, flag is not triggered if we're one value of
+ * $count below 0.500, so that we do not warn if 0.500 is not possible.
+ */
 function _ratio_helper( $count, $total )
 {
 	$ratio = 0;
 	if( $total > 0 ) {
 		$ratio = $count / $total;
 	}
-	$output = sprintf("%.3f (%d/%d)", $ratio, $count, $total);
+	$ratio = sprintf("%.3f", $ratio);
 
-	// For odd numbers of games, don't warn if we're just under an
-	// impossible-to-reach 50%.
 	$check_ratio = $ratio;
 	if( $total % 2 ) {
 		$check_ratio = ($count+1)/($total+1);
 	}
 
-	if( $check_ratio < 0.5 ) {
-		$output = "<font color='red'><b>$output</b></font>";
-	}
-	return $output;
+	return array($ratio, ($check_ratio < 0.5) );
 }
 
 
---
+++ templates/pages/league/status.tpl
@@ -0,0 +1,86 @@
+{include file=header.tpl}
+<h1>{$title}</h1>
+<p>
+	This is a general scheduling status report for rating ladder leagues
+</p>
+<table id="teams">
+    <thead>
+	<tr>
+	    <th rowspan="2">Rating</th>
+	    <th rowspan="2">Team</th>
+	    <th rowspan="2">Home&nbsp;Pct</th>
+	    <th rowspan="2">Region&nbsp;Pct</th>
+	    <th colspan="6">Games Played</th>
+	    <th rowspan="2">Opponents</th>
+	    <th rowspan="2">Repeat Opponents</th>
+	</tr>
+	<tr>
+	    <th>Total</th>
+	    <th>Home</th>
+	    <th>C</th>
+	    <th>E</th>
+	    <th>S</th>
+	    <th>W</th>
+	</tr>
+    </thead>
+    <tbody>
+    {foreach from=$teams item=team}
+	<tr>
+	    <td>{$team->rating}</td>
+	    <td><a href="{lr_url path="team/view/`$team->team_id`}">{$team->name|truncate:35}</a></td>
+	    <td {if $team->home_game_ratio_bad}style="color: white; background-color: red; font-weight: bold"{/if}>
+		{$team->home_game_ratio}
+	    </td>
+	    <td {if $team->in_region_ratio_bad}style="color: white; background-color: red; font-weight: bold"{/if}>
+		{$team->in_region_ratio}
+	    </td>
+	    <td>{$team->game_count}</td>
+	    <td>{$team->home_game_count}</td>
+	    <td {if $team->region_preference == "Central"}style="color: blue; font-weight:bold"{/if}>{$team->region_game_counts.Central|default:0}</td>
+	    <td {if $team->region_preference == "East"}style="color: blue; font-weight:bold"{/if}>{$team->region_game_counts.East|default:0}</td>
+	    <td {if $team->region_preference == "South"}style="color: blue; font-weight:bold"{/if}>{$team->region_game_counts.South|default:0}</td>
+	    <td {if $team->region_preference == "West"}style="color: blue; font-weight:bold"{/if}>{$team->region_game_counts.West|default:0}</td>
+	    <td>{$team->opponent_counts|@count}</td>
+	    <td>
+		{foreach from=$team->opponent_counts key=name item=repeats}
+		    {if $repeats > 2}
+			{$name} (<font color="red"><b>{$repeats}</b></font>)<br />
+		    {elseif $repeats > 1}
+			{$name} (<b>{$repeats}</b>)<br />
+		    {/if}
+		{/foreach}
+	    </td>
+	</tr>
+    {/foreach}
+    </tbody>
+</table>
+<script type="text/javascript">
+{literal}
+$(document).ready(function() {
+	$('#teams').dataTable( {
+		bPaginate: false,
+		bAutoWidth: false,
+		sDom: 'lfrtip',
+		bFilter: false,
+		bInfo: false,
+		bJQueryUI: true,
+		aaSorting: [[ 0, "asc" ]],
+		aoColumns: [
+			null,
+			{ "sType" : "html" },
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			{ bSortable : false }
+		]
+	} );
+})
+{/literal}
+</script>
+{include file=footer.tpl}
