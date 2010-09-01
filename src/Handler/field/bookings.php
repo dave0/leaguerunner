784ce99b8a6e92dd58565551ae97f4a63e9c8689
Dave O'Neill <dmo@dmo.ca>
2010-09-01 18:14:55 -0400

    Templating: field/bookings

--- src/Handler/field/bookings.php
+++ src/Handler/field/bookings.php
@@ -6,11 +6,6 @@ class field_bookings extends FieldHandler
 	function has_permission()
 	{
 		global $lr_session;
-
-		if ($this->field->status != 'open') {
-			error_exit("That field is closed");
-		}
-
 		return $lr_session->has_permission('field','view', $this->field->fid);
 	}
 
@@ -18,34 +13,29 @@ class field_bookings extends FieldHandler
 	{
 		global $lr_session;
 
-		$this->title = "Bookings: {$this->field->fullname}";
+		$this->title = "{$this->field->fullname} &raquo; Bookings";
+
+		$this->template_name = 'pages/field/bookings.tpl';
+
+		if ($this->field->status != 'open') {
+			error_exit("That field is closed");
+		}
 
 		$sth = GameSlot::query( array('fid' => $this->field->fid,
 				'_extra' => 'DATE_SUB(CURDATE(), INTERVAL 1 YEAR) AND DATE_ADD(CURDATE(), INTERVAL 1 YEAR)',
 				'_order' => 'g.game_date, g.game_start'));
 
-		$header = array("Date","Start Time","End Time","Booking", "Actions");
-		$rows = array();
+		$slots = array();
 		while($slot = $sth->fetchObject('GameSlot') ) {
-			$booking = '';
-			$actions = array();
-			if( $lr_session->has_permission('gameslot','edit', $slot->slot_id)) {
-				$actions[] = l('change avail', "slot/availability/$slot->slot_id");
-			}
-			if( $lr_session->has_permission('gameslot','delete', $slot->slot_id)) {
-				$actions[] = l('delete', "slot/delete/$slot->slot_id");
-			}
 			if($slot->game_id) {
-				$game = Game::load( array('game_id' => $slot->game_id) );
-				$booking = l($game->league_name,"game/view/$slot->game_id");
-				if( $lr_session->has_permission('game','reschedule', $slot->game_id)) {
-					$actions[] = l('reschedule/move', "game/reschedule/$slot->game_id");
-				}
+				$slot->game = Game::load( array('game_id' => $slot->game_id) );
 			}
-			$rows[] = array($slot->game_date, $slot->game_start, $slot->display_game_end(), $booking, implode(' | ', $actions));
+			$slots[] = $slot;
 		}
 
-		return "<div class='listtable'>" . table($header, $rows) . "</div>";
+		$this->smarty->assign('slots', $slots);
+
+		return true;
 	}
 }
 ?>
---
+++ templates/pages/field/bookings.tpl
@@ -0,0 +1,58 @@
+{include file=header.tpl}
+<h1>{$title}</h1>
+<script type="text/javascript">
+{literal}
+	$(document).ready(function() {
+		$('#bookings').dataTable( {
+			"bAutoWidth": false,
+			"sDom": 'lfrtip',
+			"bJQueryUI": true,
+			"bPaginate": false,
+			"bFilter": false,
+			"bInfo": false,
+			"aoColumns": [
+				null,
+				null,
+				null,
+				{ "sType" : "html" },
+				{ bSortable : false }
+			]
+		});
+	});
+{/literal}
+</script>
+
+<table id="bookings">
+<thead>
+	<tr>
+		<th>Date</th>
+		<th>Start Time</th>
+		<th>End Time</th>
+		<th>Booking</th>
+		<th>Actions</th>
+	</tr>
+</thead>
+<tbody>
+{foreach from=$slots item=s}
+<tr>
+  <td>{$s->game_date}</td>
+  <td>{$s->game_start}</td>
+  <td>{$s->display_game_end()}</td>
+  <td>{if $s->game}<a href="{lr_url path="game/view/`$s->game_id`"}">{$s->game->league_name}</a>{/if}</td>
+  <td>
+  	{if session_perm("gameslot/edit/`$s->slot_id`")}
+	<a href="{lr_url path="slot/availability/`$s->slot_id`"}">change avail</a> |
+	{/if}
+  	{if session_perm("gameslot/delete/`$s->slot_id`")}
+	<a href="{lr_url path="slot/delete/`$s->slot_id`"}">delete</a> |
+	{/if}
+  	{if session_perm("game/reschedule/`$s->slot_id`")}
+	<a href="{lr_url path="game/reschedule/`$s->game_id`"}">reschedule</a>
+	{/if}
+  </td>
+</tr>
+{/foreach}
+</tbody>
+<tfoot></tfoot>
+</table>
+{include file=footer.tpl}
