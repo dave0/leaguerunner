0b498481b30a208aee22e5fb02d0bbc0f90dd928
Dave O'Neill <dmo@dmo.ca>
2010-11-21 14:25:01 -0500

    Templating: slot/availability

--- src/Handler/slot/availability.php
+++ src/Handler/slot/availability.php
@@ -10,41 +10,33 @@ class slot_availability extends SlotHandler
 
 	function process()
 	{
+		global $dbh;
 		$edit = &$_POST['edit'];
 
+		$this->template_name = 'pages/slot/availability.tpl';
 		$this->title = "{$this->slot->field->fullname} &raquo; Gameslot {$this->slot->slot_id} Availability";
-		switch($edit['step']) {
-			case 'perform':
-				foreach( $this->slot->leagues as $league) {
-					if( !count( $edit['availability']) || !in_array( $league->league_id, $edit['availability']) ) {
-						$this->slot->remove_league( $league );
-					}
+
+		if( $edit['step'] == 'perform' ) {
+			foreach( $this->slot->leagues as $league) {
+				if( !count( $edit['availability']) || !in_array( $league->league_id, $edit['availability']) ) {
+					$this->slot->remove_league( $league );
 				}
-				if( count($edit['availability']) > 0 ) {
-					foreach ( $edit['availability'] as $league ) {
-						$this->slot->add_league($league);
-					}
+			}
+			if( count($edit['availability']) > 0 ) {
+				foreach ( $edit['availability'] as $league ) {
+					$this->slot->add_league($league);
 				}
-				if( ! $this->slot->save() ) {
-					error_exit("Internal error: couldn't save gameslot");
-				}
-				local_redirect(url("slot/availability/" . $this->slot->slot_id));
-				break;
-			default:
-				return $this->generateForm( $this->slot );
+			}
+			if( ! $this->slot->save() ) {
+				error_exit("Internal error: couldn't save gameslot");
+			}
+
+			$this->smarty->assign('message', 'Changes saved');
 		}
-	}
 
-	function generateForm()
-	{
-		global $dbh;
-		# What day is this weekday?
-		$weekday = strftime("%A", $this->slot->date_timestamp);
-
-		$output = "<p>Availability for " . $this->slot->game_date . " " . $this->slot->game_start. "</p>";
+		$this->smarty->assign('slot', $this->slot);
 
 		$leagues = array();
-		// TODO: Pull into get_league_checkbox();
 		$sth = $dbh->prepare("SELECT
 			l.league_id,
 			l.name,
@@ -53,7 +45,9 @@ class slot_availability extends SlotHandler
 			WHERE l.schedule_type != 'none'
 			AND (FIND_IN_SET(?, l.day) > 0)
 			AND l.status = 'open'
-			ORDER BY l.day,l.name,l.tier");
+			ORDER BY l.day,l.name,l.tier"
+		);
+		$weekday = strftime("%A", $this->slot->date_timestamp);
 		$sth->execute( array( $weekday) );
 
 		while($league = $sth->fetch(PDO::FETCH_OBJ) ) {
@@ -62,24 +56,18 @@ class slot_availability extends SlotHandler
 			} else {
 				$league->fullname = $league->name;
 			}
-			$league->selected = false;
-			$leagues[$league->league_id] = $league;
+			$leagues[$league->league_id] = $league->fullname;
 		}
 
+		$this->smarty->assign('leagues', $leagues);
+
+		$current_selections = array();
 		foreach($this->slot->leagues as $league) {
-			$leagues[$league->league_id]->selected = true;
+			$current_selections[] = $league->league_id;
 		}
+		$this->smarty->assign('current_leagues', $current_selections);
 
-		while(list($id, $league) = each($leagues) ) {
-			$chex .= form_checkbox($league->fullname, 'edit[availability][]', $id, $league->selected);
-		}
-
-		$chex .= form_submit('submit') . form_reset('reset');
-		$output .= form_hidden('edit[step]', 'perform');
-
-		$output .= form_group('Make Gameslot Available To:', $chex);
-
-		return form($output);
+		return true;
 	}
 }
 
---
+++ templates/pages/slot/availability.tpl
@@ -0,0 +1,13 @@
+{include file=header.tpl}
+<h2>{$title}</h1>
+<p>Availability for {$slot->game_date} {$slot->game_start}</p>
+<form method="POST" id="availability">
+<input type="hidden" name="edit[step]" value="perform" />
+{include file=components/errormessage.tpl message=$message}
+<fieldset><legend>Make Gameslot Available To:</legend>
+{html_checkboxes labels=false name="edit[availability]" options=$leagues selected=$current_leagues separator="<br />"}
+</fieldset>
+<input type="submit" name="submit" value="submit" />
+<input type="reset" name="reset" value="reset" />
+</form>
+{include file=footer.tpl}
