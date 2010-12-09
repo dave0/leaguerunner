54badf0a2ea3749236996cb6888aee1c552aa1cf
Dave O'Neill <dmo@dmo.ca>
2010-12-09 12:04:53 -0500

    Bugfix: API schedule/gamestoday used wrong date

--- src/Handler/api/1/schedule/gamestoday.php
+++ src/Handler/api/1/schedule/gamestoday.php
@@ -14,12 +14,14 @@ class api_1_schedule_gamestoday extends Handler
 
 		$this->template_name = 'api/1/schedule/gamestoday.tpl';
 
+		$now = time();
+
 		$sth = $dbh->prepare('SELECT COUNT(*), COUNT(DISTINCT(game_end)) from gameslot WHERE game_date = ? AND NOT ISNULL(game_id)');
 		$sth->execute( array( strftime('%Y-%m-%d', $now) ) );
 		list($game_count, $distinct_end_times) = $sth->fetch();
 
 		$this->smarty->assign('game_count', $game_count);
-		$this->smarty->assign('timestamp', time());
+		$this->smarty->assign('timestamp', $now);
 
 		/* Also, take a stab at guessing timecap.  Since only summer usually
 		 * has a "default" timecap, we will only display for that season.
